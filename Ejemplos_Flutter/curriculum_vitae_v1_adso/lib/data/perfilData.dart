816c8cdf6e95cb7f78e2c71152ad271c46f33470
Jeferson <jefer.hernandez1@gmail.com>
2024-11-20 20:55:59 -0500

    Curriculum Vitae V2 CHALLENGUE Finalizado

--- Ejemplos_Flutter/curriculum_vitae_v1_adso/lib/data/perfilData.dart
+++ Ejemplos_Flutter/curriculum_vitae_v1_adso/lib/data/perfilData.dart
@@ -3,7 +3,7 @@ Map<String,dynamic> perfilDataList = {
   "celular" : "3113975576",
   "dirección" : "Calle 123 # 14 -27",
   "gitHub" : "@jeferson-hernandez08",
-  "perfil" : "Desarrollador de software con más de [X] años de experiencia en el diseño, desarrollo e implementación de soluciones tecnológicas innovadoras. Experto en lenguajes de programación como [Java, Python, JavaScript, etc.] y en el uso de frameworks y tecnologías modernas como [React, Node.js, Django, etc.]. Con un enfoque en la creación de aplicaciones escalables, optimización de procesos y resolución eficiente de problemas, trabajando siempre con metodologías ágiles (Scrum, Kanban) para asegurar la entrega de proyectos a tiempo. Apasionado por la mejora continua y la integración de nuevas tecnologías, con habilidades de trabajo en equipo, comunicación efectiva y una sólida orientación hacia el cliente."
+  "perfil" : "Desarrollador de software con más de 12 años de experiencia en el diseño, desarrollo e implementación de soluciones tecnológicas innovadoras. Experto en lenguajes de programación como [Java, Python, JavaScript, etc.] y en el uso de frameworks y tecnologías modernas como [React, Node.js, Django, etc.]. Con un enfoque en la creación de aplicaciones escalables, optimización de procesos y resolución eficiente de problemas, trabajando siempre con metodologías ágiles (Scrum, Kanban) para asegurar la entrega de proyectos a tiempo. Apasionado por la mejora continua y la integración de nuevas tecnologías, con habilidades de trabajo en equipo, comunicación efectiva y una sólida orientación hacia el cliente."
 };
 
 // Consulta GPT:
--- Ejemplos_Flutter/curriculum_vitae_v1_adso/lib/perfilPersonal/perfilPersonal.dart
+++ Ejemplos_Flutter/curriculum_vitae_v1_adso/lib/perfilPersonal/perfilPersonal.dart
@@ -37,25 +37,234 @@ class _PerfilPersonalState extends State<PerfilPersonal> {
                       fit: BoxFit.cover,
                       width: 150,
                       height: 150,
-
                     ),
-
                   ),
                 ),
               ),
               Card(
-                child: Text("Información Personal")
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          'Jeferson Mauricio Hernandez Ladino',
+                          style: TextStyle(fontSize: 11),
+                        ),
+                        leading: Icon(Icons.person),
+                      ),
+                     Divider(
+                        //thickness: 1,    // Grosor de la línea (Por defecto viene de 0.5)
+                        height: 0,        // Espacio por encima y debajo del divisor (Por defecto viene de 16.0)
+                        indent: 15,      // Espacio antes de la línea (hace que la línea empiece más a la derecha)(Ambos por defrecto es de 0.0 ocupa todo el ancho del contenedor)
+                        endIndent: 20,  // Espacio después de la línea (hace que la línea termine más antes de llegar al borde)
+                        color: Colors.green[500],
+                      ),
+                      ListTile(
+                        title: Text(
+                          '3113975576',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        leading: Icon(Icons.phone_android),
+                      ),
+                      Divider(
+                        //thickness: 1,    
+                        height: 0,        
+                        indent: 15,   
+                        endIndent: 20, 
+                        color: Colors.green[500],
+                      ),
+                      ListTile(
+                        title: Text(
+                          'Calle 123 E # 41 - 51',
+                          style: TextStyle(fontSize: 10),
+                        ),
+                        leading: Icon(Icons.location_on),
+                      ),
+                      Divider(
+                        //thickness: 1,    
+                        height: 0,        
+                        indent: 15,   
+                        endIndent: 20, 
+                        color: Colors.green[500],
+                      ),
+                      ListTile(
+                        title: Text(
+                          '@jefer.hernandez1',
+                          style: TextStyle(fontSize: 11),
+                        ),
+                        leading: Icon(Icons.mail),
+                      ),
+                    ],
+                  ),
+                ), 
+              ),  
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
+                    children: [
+                      Text(
+                        'Dark',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.8, // 80% de progreso
+                        minHeight: 5, // Altura de la barra
+                        color: Colors.green, // Color de la barra
+                        backgroundColor: Colors.grey[300], // Color de fondo de la barra
+                      ),
+                      SizedBox(height: 10), // Espacio de 10 píxeles entre los elementos. El widget SizedBox permite especificar un tamaño fijo para el espacio entre los elementos.
+
+                      Text(
+                        'Flutter',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.7, 
+                        minHeight: 5, 
+                        color: Colors.green, 
+                        backgroundColor: Colors.grey[300], 
+                      ),
+                      SizedBox(height: 10,),
+
+                      Text(
+                        'JavaScript',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.9, 
+                        minHeight: 5, 
+                        color: Colors.green, 
+                        backgroundColor: Colors.grey[300], 
+                      ),
+                      SizedBox(height: 10,),
+
+                      Text(
+                        'PHP',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.9, 
+                        minHeight: 5, 
+                        color: Colors.green, 
+                        backgroundColor: Colors.grey[300], 
+                      ),
+                      SizedBox(height: 10,),
+
+                      Text(
+                        'Python',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.75, 
+                        minHeight: 5, 
+                        color: Colors.green, 
+                        backgroundColor: Colors.grey[300], 
+                      ),
+                      SizedBox(height: 10,),
+
+                      Text(
+                        'React',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.9, 
+                        minHeight: 5, 
+                        color: Colors.green, 
+                        backgroundColor: Colors.grey[300], 
+                      ),
+                      SizedBox(height: 10,),
+
+                      Text(
+                        'SQL',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.9, 
+                        minHeight: 5, 
+                        color: Colors.green, 
+                        backgroundColor: Colors.grey[300], 
+                      ),
+                      SizedBox(height: 10,),
+                    ],
+                  ),
+                ),
               ),
               Card(
-                child: Text("Skills"),
-              ),
-              Card(
-                child: Text("idiomas"),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
+                    children: [
+                      Text(
+                        'Reading',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.8, // 80% de progreso
+                        minHeight: 5, // Altura de la barra
+                        color: Colors.blue, // Color de la barra
+                        backgroundColor: Colors.grey[300], // Color de fondo de la barra
+                      ),
+                      SizedBox(height: 10), // Espacio de 10 píxeles entre los elementos. El widget SizedBox permite especificar un tamaño fijo para el espacio entre los elementos.
+
+                      Text(
+                        'Speaking',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.7, 
+                        minHeight: 5, 
+                        color: Colors.blue, 
+                        backgroundColor: Colors.grey[300], 
+                      ),
+                      SizedBox(height: 10,),
+
+                      Text(
+                        'Listening',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.9, 
+                        minHeight: 5, 
+                        color: Colors.blue, 
+                        backgroundColor: Colors.grey[300], 
+                      ),
+                      SizedBox(height: 10,),
+
+                      Text(
+                        'Writing',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      LinearProgressIndicator(
+                        value: 0.9, 
+                        minHeight: 5, 
+                        color: Colors.blue, 
+                        backgroundColor: Colors.grey[300], 
+                      ),
+                      SizedBox(height: 10,),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
-          ListTile(
-            title: Text(perfilDataList["perfil"]),
+          Card(
+            color: Colors.white,
+            shadowColor:Color.fromARGB(255, 0, 0, 0),
+            elevation: 20,
+            surfaceTintColor: Colors.white,
+            margin: EdgeInsets.all(7),
+
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: Text(
+                  perfilDataList["perfil"],
+                  style: TextStyle(fontSize: 11),
+                ),
+              ),
+            ),
           ),
         ],
       ),
